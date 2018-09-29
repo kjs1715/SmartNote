1c6bc019583d80873d96c973194526cbf802af25
廖远达 <2016011245@secoder.net>
2018-09-29 09:49:46 +0000

    XFBean.java: 由AudioInterface类使用

---
+++ XFBean.java
@@ -0,0 +1,13 @@
+package com.example.liaoyuanda.ifly_5_0;
+
+import java.util.ArrayList;
+
+public class XFBean {
+    public ArrayList<WS> ws;
+    public class WS{
+        public ArrayList<CW> cw;
+    }
+    public class CW{
+        public String w;
+    }
+}
