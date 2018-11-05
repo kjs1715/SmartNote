eae242b13d22f52ba02e3ab808b33312f1c5913b
kjs1715 <kjsbuzz0506@gmail.com>
2018-11-05 18:50:40 +0800

    Lifecycle testing for all activity

---
+++ app/src/test/java/com/littleboss/smartnote/SearchActivityTest.java
@@ -0,0 +1,37 @@
+package com.littleboss.smartnote;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.littleboss.smartnote.Utils.ImageUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ImageUtil;
+import static org.junit.Assert.*;
+
+
+@RunWith(RobolectricTestRunner.class)
+public class SearchActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void startTest() throws Exception {
+        ActivityController<SearchActivity> controller = Robolectric.buildActivity(SearchActivity.class).create().start().resume().visible();
+        Activity activity = controller.get();
+        assertNotNull(activity);
+    }
+}
