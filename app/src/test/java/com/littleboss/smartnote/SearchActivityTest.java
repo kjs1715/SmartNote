35a65beb55c91d893453d4107a09c0f90c7e9576
kjs1715 <kjsbuzz0506@gmail.com>
2018-11-10 00:06:19 +0800

    Having coverage of 100% of methods and 98% of lines with SearchActivity

--- app/src/test/java/com/littleboss/smartnote/SearchActivityTest.java
+++ app/src/test/java/com/littleboss/smartnote/SearchActivityTest.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.littleboss.smartnote.Utils.ImageUtils;
@@ -16,9 +19,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ImageUtil;
+import org.robolectric.shadows.ShadowListView;
+
 import static org.junit.Assert.*;
 
 
@@ -29,17 +35,26 @@ public class SearchActivityTest {
         NoteDatabase.dropDatabaseIfExist();
         NoteDatabase database = NoteDatabase.getInstance();
         database.setTestMod(1);
+        // for testing, inserted a data for database, inorder to return notesList
+        NoteDatabase.saveNoteByTitle("", "test", "test");
     }
 
-    @After
-    public void afterTest() {
-        NoteDatabase.closeConnection();
-    }
+//    @After
+//    public void afterTest() {
+//        NoteDatabase.closeConnection();
+//    }
 
     @Test
     public void startTest() throws Exception {
         ActivityController<SearchActivity> controller = Robolectric.buildActivity(SearchActivity.class).create().start().resume().visible();
         Activity activity = controller.get();
         assertNotNull(activity);
+
+        Button button = activity.findViewById(R.id.searchbutton);
+        button.performClick();
+
+        ListView listView = activity.findViewById(R.id.searchlistview);
+        ShadowListView shadowListView = Shadows.shadowOf(listView);
+        shadowListView.performItemClick(0);
     }
 }
